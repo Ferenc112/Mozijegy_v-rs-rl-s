ed8f618b6015e521c5751b3c66a3fb03984b7caf
KittyPetCat <diaadam07@gmail.com>
2026-04-21 09:59:08 +0200

    igena

--- valasz.js
+++ valasz.js
@@ -11,7 +11,7 @@ function filmbetoltese() {
         kartya.classList.add("film-kartya");
 
         kartya.innerHTML = `
-          <img src="${film.kep_url}" alt="${film.nev}" style="width: 100%; height: 150px; object-fit: cover; border-radius: 5px; margin-bottom: 10px;">
+          <img class="kepek" src="${film.kep_url}" alt="${film.nev}" style="width: 100%; height: 150px; object-fit: cover; border-radius: 5px; margin-bottom: 10px;">
           <h3>${film.nev}</h3>
           <p><strong>Producer:</strong> ${film.producer}</p>
           <p><strong>Hossz:</strong> ${film.hossz_perc} perc</p>
